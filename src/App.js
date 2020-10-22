84a7d07c31e0f5ce6191974af60ff42e1ae06aaa
Nicolas Posa <nicoposa57@gmail.com>
2020-10-22 16:39:30 -0300

    create ListOfCategories and GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import { Category } from './components/category/index'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => (
-  <Category />
+  <div className="">
+    <GlobalStyle />
+    <ListOfCategories />
+  </div>
 )
