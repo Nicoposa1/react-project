e4a83ed3177bb9fba9ce566d2c6396712b6efe43
Nicolas Posa <nicoposa57@gmail.com>
2020-11-06 23:34:50 -0300

    Utilizando intesction observer para que no renderizen todas las fotos al mismo tiempo

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,41 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { Fragment, useEffect, useRef, useState  } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGES = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGES }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper className="">
-          <Img src={src} alt=""/>
-        </ImgWrapper>
-      </a>
+  const ref = useRef(null)
+  const [ show, setShow ] = useState(false)
 
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes
-      </Button>
-    </article>
+  useEffect(function(){
+    const observer = new window.IntersectionObserver(function(entries){
+      const { isIntersecting } = entries[0]
+      console.log(isIntersecting);
+      if(isIntersecting){
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+
+
+  return (
+    <Article ref={ref}>
+      {
+        show && <Fragment>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper className="">
+              <Img src={src} alt=""/>
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes
+          </Button>
+        </Fragment>
+      }
+    </Article>
   )
 }
