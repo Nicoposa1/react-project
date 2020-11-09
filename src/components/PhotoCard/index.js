bfc6babf9bec111f9bf7963e1bf5e348add6d210
Nicolas Posa <nicoposa57@gmail.com>
2020-11-09 20:55:06 -0300

    agregando babel-eslint y pollyfill de intersection obeserver

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useRef, useState  } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
 
 import { MdFavoriteBorder } from 'react-icons/md'
@@ -6,35 +6,41 @@ import { MdFavoriteBorder } from 'react-icons/md'
 const DEFAULT_IMAGES = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGES }) => {
   const ref = useRef(null)
-  const [ show, setShow ] = useState(false)
+  const [show, setShow] = useState(false)
 
-  useEffect(function(){
-    const observer = new window.IntersectionObserver(function(entries){
-      const { isIntersecting } = entries[0]
-      console.log(isIntersecting);
-      if(isIntersecting){
-        setShow(true)
-        observer.disconnect()
-      }
-    })
-    observer.observe(ref.current)
+  useEffect(function () {
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    )
+      .then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setShow(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(ref.current)
+      })
   }, [ref])
-
 
   return (
     <Article ref={ref}>
       {
-        show && <Fragment>
-          <a href={`/detail/${id}`}>
-            <ImgWrapper className="">
-              <Img src={src} alt=""/>
-            </ImgWrapper>
-          </a>
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper className=''>
+                <Img src={src} alt='' />
+              </ImgWrapper>
+            </a>
 
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes
-          </Button>
-        </Fragment>
+            <Button>
+              <MdFavoriteBorder size='32px' /> {likes} likes
+            </Button>
+          </>
       }
     </Article>
   )
