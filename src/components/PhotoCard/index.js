a335693974e2ba4c69a9b351f511570f9baf1e18
Nicolas Posa <nicoposa57@gmail.com>
2020-12-07 17:11:08 -0300

    agregando el navbar en la aplicación y el Link en reach router

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { ImgWrapper, Img, Button, Article } from './styles'
+import { ImgWrapper, Img, Article } from './styles'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton/index'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-
   const [show, element] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
@@ -20,18 +20,19 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
           <>
-            <a href={`/?detail=${id}`}>
+            <Link to={`/detail/${id}`}>
               <ImgWrapper>
                 <Img src={src} />
               </ImgWrapper>
-            </a>
+            </Link>
             <ToggleLikeMutation>
               {
                 (toggleLike) => {
                   const handleFavClick = () => {
-                    !liked && toggleLike({ variables: {
-                      input: { id }
-                    }
+                    !liked && toggleLike({
+                      variables: {
+                        input: { id }
+                      }
                     })
                     setLiked(!liked)
                   }
