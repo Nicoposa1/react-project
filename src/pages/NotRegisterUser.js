457898043f5c7c426575901cc67cfbc07f09c65c
Nicolas Posa <nicoposa57@gmail.com>
2020-12-29 16:46:09 -0300

    creando el login de la aplicación

--- src/pages/NotRegisterUser.js
+++ src/pages/NotRegisterUser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisterUser = () => (
   <Context.Consumer>
@@ -18,12 +19,25 @@ export const NotRegisterUser = () => (
                     register({ variables }).then(activateAuth)
                   }
 
-                  const ErrorMsg = error && 'El usuario y a esta registrado o hubo algun error'
-                  return <UserForm disabled={loading} error={ErrorMsg} title='Registrarse' onSubmit={onSubmit} />
+                  const errorMsg = error && 'El usuario y a esta registrado o hubo algun error'
+                  return <UserForm disabled={loading} error={errorMsg} title='Registrarse' onSubmit={onSubmit} />
                 }
               }
             </RegisterMutation>
-            <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+            <LoginMutation>
+              {
+                (login, { data, loading, error }) => {
+                  const onSubmit = ({ email, password }) => {
+                    const input = { email, password }
+                    const variables = { input }
+                    login({ variables }).then(activateAuth)
+                  }
+
+                  const errorMsg = error && 'La contraseña no es correcta o el usuario no existe'
+                  return <UserForm disabled={loading} error={errorMsg} title='Iniciar Sesión' onSubmit={onSubmit} />
+                }
+              }
+            </LoginMutation>
           </>
         )
       }
