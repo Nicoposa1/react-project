ee8fe26eb046adcc78fec0c67613b44a5c5c7c93
Nicolas Posa <nicoposa57@gmail.com>
2021-01-19 22:51:05 -0300

    soporte offline con workbox

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifest = require('webpack-pwa-manifest')
 const path = require('path')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   output: {
@@ -14,6 +15,7 @@ module.exports = {
     new WebpackPwaManifest({
       name: 'Petgram - Tu app de fotos de mascotas',
       short_name: 'Petgram',
+      start_url: '/',
       description: 'Con Petgram puedes encontrar fotos de animales mas lindos',
       background_color: '#fff',
       theme_color: '#b1a',
@@ -21,6 +23,35 @@ module.exports = {
         {
           src: path.resolve('src/assets/icon.png'),
           size: [96, 128, 192, 256, 384, 512]
+        },
+        {
+          src: path.resolve('src/assets/maskable-icon.png'),
+          size: '1024x1024',
+          purpose: 'maskable'
+        },
+        {
+          src: path.resolve('src/assets/icons/ios-icon.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          destination: path.join('icons', 'ios'),
+          ios: true
+        }
+      ]
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          urlPattern: new RegExp('https://https://react-project-silk.vercel.app/'),
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
